a8d7e6a2a8cd054272a1991f49b13d79510cd9d2
Philip Duffy <willmanduffy@gmail.com>
2014-08-11 13:20:06 -0400

    Prevent site from crashing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :find_last_watched
+  # FIXME: Server is down
+  # before_filter :find_last_watched
 
   private
 
